fdbe5425a35872a58e426d011fb415ebc9c2ed43
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-03-16 01:16:33 +0200

    Add ReactorPoolIsolatedBugTest

--- src/test/java/com/example/ReactorPoolIsolatedBugTest.java
+++ src/test/java/com/example/ReactorPoolIsolatedBugTest.java
@@ -10,11 +10,8 @@ import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class UsingWhenBugTest {
+public class ReactorPoolIsolatedBugTest {
 
     private static final int COUNT = 10_000;
 
@@ -47,20 +44,11 @@ public class UsingWhenBugTest {
         );
 
         CountDownLatch cdl = new CountDownLatch(COUNT);
-
-        AtomicInteger acquireCount = new AtomicInteger();
-        AtomicInteger releaseCount = new AtomicInteger();
-        AtomicInteger errorReleaseCount = new AtomicInteger();
-        AtomicInteger cancelReleaseCount = new AtomicInteger();
-
         for (int i = 0; i < COUNT; i++) {
-            executorService.submit(new FlatMapErrorTask(cdl, acquireCount, releaseCount, errorReleaseCount, cancelReleaseCount));
+            executorService.submit(new FlatMapErrorTask(cdl));
         }
 
         cdl.await();
-
-        assertEquals(10 * COUNT, acquireCount.get());
-        assertEquals(10 * COUNT, releaseCount.get() + errorReleaseCount.get() + cancelReleaseCount.get());
     }
 
     private static final class PoolMetricsLogger implements Runnable {
@@ -89,38 +77,25 @@ public class UsingWhenBugTest {
         }
     }
 
-    private static final class FlatMapErrorTask implements Runnable {
+    private final class FlatMapErrorTask implements Runnable {
 
         private final CountDownLatch cdl;
-        private final AtomicInteger acquireCount;
-        private final AtomicInteger releaseCount;
-        private final AtomicInteger errorReleaseCount;
-        private final AtomicInteger cancelReleaseCount;
 
-        public FlatMapErrorTask(CountDownLatch cdl, AtomicInteger acquireCount, AtomicInteger releaseCount, AtomicInteger errorReleaseCount, AtomicInteger cancelReleaseCount) {
+        public FlatMapErrorTask(CountDownLatch cdl) {
             this.cdl = cdl;
-            this.acquireCount = acquireCount;
-            this.releaseCount = releaseCount;
-            this.errorReleaseCount = errorReleaseCount;
-            this.cancelReleaseCount = cancelReleaseCount;
         }
 
         public void run() {
             Flux<Void> flux = Flux
                     .range(0, 10)
-                    .flatMap(i -> Flux
-                            .usingWhen(
-                                    Mono
-                                            .fromSupplier(acquireCount::getAndIncrement)
-                                            .delayElement(Duration.ofMillis(2))
-                                            .thenReturn("value"),
-                                    value -> Mono
-                                            .just(value)
-                                            .delayElement(Duration.ofMillis(10))
-                                            .then(),
-                                    value -> Mono.fromSupplier(releaseCount::getAndIncrement).then(),
-                                    (value, error) -> Mono.fromSupplier(errorReleaseCount::getAndIncrement).then(),
-                                    value -> Mono.fromSupplier(cancelReleaseCount::getAndIncrement).then()
+                    .flatMap(i -> stringReactivePool
+                            .acquire()
+                            .flatMap(pooledRef -> Mono
+                                    .just(pooledRef.poolable())
+                                    .delayElement(Duration.ofMillis(10))
+                                    .then(Mono.defer(() -> pooledRef.release()))
+                                    .onErrorResume(error -> pooledRef.release())
+                                    .doOnCancel(() -> pooledRef.release().subscribe())
                             )
                             .switchIfEmpty(Mono.error(new RuntimeException("Empty")))
                     )
