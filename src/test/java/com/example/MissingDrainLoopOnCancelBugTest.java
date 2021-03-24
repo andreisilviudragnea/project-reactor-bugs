ab392d56dfcb09fc99d908e5164f9e02075e15d8
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2021-03-24 12:22:10 +0200

    Add MissingDrainLoopOnCancelBugTest

---
+++ src/test/java/com/example/MissingDrainLoopOnCancelBugTest.java
@@ -0,0 +1,90 @@
+package com.example;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.Mono;
+import reactor.pool.InstrumentedPool;
+import reactor.pool.PoolBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class MissingDrainLoopOnCancelBugTest {
+
+	private final InstrumentedPool<String> stringReactivePool = PoolBuilder
+			.from(Mono.just("value").delayElement(Duration.ofMinutes(10)))
+			.maxPendingAcquireUnbounded()
+			.sizeBetween(0, 1)
+			.buildPool();
+
+	@Test
+	public void acquiredSizeBug() {
+		InstrumentedPool.PoolMetrics metrics = stringReactivePool.metrics();
+
+		AtomicReference<Subscription> subscriptionReference = new AtomicReference<>();
+
+		AtomicBoolean next1 = new AtomicBoolean();
+
+		stringReactivePool.acquire().subscribe(
+				stringPooledRef -> next1.set(true),
+				throwable -> System.out.println("error1"),
+				() -> System.out.println("complete1"),
+				subscription -> {
+					subscriptionReference.set(subscription);
+					subscription.request(Long.MAX_VALUE);
+				}
+		);
+
+		assertFalse(next1.get());
+
+		assertEquals(metrics.acquiredSize(), 1); // should be 0
+	}
+
+	@Test
+	public void missingDrainLoopOnCancelBug() {
+		InstrumentedPool.PoolMetrics metrics = stringReactivePool.metrics();
+
+		AtomicReference<Subscription> subscriptionReference = new AtomicReference<>();
+
+		AtomicBoolean next1 = new AtomicBoolean();
+
+		stringReactivePool.acquire().subscribe(
+				stringPooledRef -> next1.set(true),
+				throwable -> System.out.println("error1"),
+				() -> System.out.println("complete1"),
+				subscription -> {
+					subscriptionReference.set(subscription);
+					subscription.request(Long.MAX_VALUE);
+				}
+		);
+
+		assertFalse(next1.get());
+
+		AtomicBoolean next2 = new AtomicBoolean();
+
+		stringReactivePool.acquire().subscribe(
+				stringPooledRef -> next2.set(true),
+				throwable -> System.out.println("error2"),
+				() -> System.out.println("complete2")
+		);
+
+		assertFalse(next1.get());
+		assertFalse(next2.get());
+
+		System.out.println("before cancel");
+
+		subscriptionReference.get().cancel();
+
+		assertFalse(next1.get());
+		assertFalse(next2.get()); // should be true
+
+		System.out.println("after cancel");
+
+		assertEquals(metrics.acquiredSize(), 1); // should be 0 for current implementation, given that no acquire is successful
+		assertEquals(metrics.pendingAcquireSize(), 1); // should be 0
+	}
+}
